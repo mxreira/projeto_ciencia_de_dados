12f056a05046805dc91d4f7cc9d8bc24a89dc5de
Gabriel de Freitas Moreira <gabriel.defreitasmoreira@gmail.com>
2025-12-15 15:05:52 -0300

    Conclusão do projeto e deploy no streamlit

--- DeployProjetoAirbnb.py
+++ DeployProjetoAirbnb.py
@@ -2,14 +2,11 @@ import pandas as pd
 import streamlit as st
 import joblib
 
-# modelo = joblib.load('modelo.joblib')
-
 df_dados = pd.read_csv('dados.csv')
 
 df_dados.drop('Unnamed: 0', axis=1, inplace= True)
 df_dados.drop('price', axis=1, inplace= True)
-atributos = df_dados.columns
-
+atributos = list(df_dados.columns)
 
 x_numericos_keys = ['host_listings_count', 'latitude', 'longitude', 'accommodates', 'bathrooms', 'bedrooms', 'beds', 'extra_people', 'minimum_nights', 'month', 'year', 'n_amenities']
 
@@ -50,14 +47,16 @@ for col in df_dados:
         else:
             x_listas['cancellation_policy'] = [categoria]
 
-    
-print(x_listas)
 
 for item in x_numericos:
 
     if item == 'latitude' or item == 'longitude':
         valor = st.number_input(f'{item}', step= 0.00001, value= 0.0, format='%.5f')
-    valor = st.number_input(f'{item}')
+    elif item == 'extra_people':
+        valor = st.number_input(f'{item}', step= 0.01, value= 0.0, format='%.2f')
+    else:
+        valor = st.number_input(f'{item}', step= 1, value= 0)
+    
     x_numericos[item] = valor
 
 for item in x_tf:
@@ -65,11 +64,34 @@ for item in x_tf:
 
     x_tf[item] = 1 if valor == 'Sim' else 0
 
-for item, lista_opcoes in x_listas.items():
+dicionario = {}
 
+for item, lista_opcoes in x_listas.items():
+    
     valor = st.selectbox(f'{item}', lista_opcoes)
+
+    for opcao in lista_opcoes:
+        coluna_dummy = f'{item}_{opcao}'
+        
+        if valor == opcao:
+
+            dicionario[coluna_dummy] = 1
+        else:
+            dicionario[coluna_dummy] = 0
+
 
 botao = st.button('Prever Valor do Imóvel')
 
+if botao:
+    dicionario.update(x_numericos)
+    dicionario.update(x_tf)
+    
+    valores_x = pd.DataFrame(dicionario, index=[0])
+    
+    valores_x = valores_x[atributos]  # Reordenando colunas de maneira simplificada
 
+    modelo = joblib.load('modelo.joblib')
 
+    preco = modelo.predict(valores_x)
+
+    st.write(f'O preço da diária do imóvel é estimado em: R$ {preco[0]}')
